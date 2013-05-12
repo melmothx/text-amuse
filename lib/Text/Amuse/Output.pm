40b5f4a081b268e42aff6d94c8761325c449dea8
Marco Pessotto <melmothx@gmail.com>
2013-05-12 19:57:09 +0200

    Removed invalid call to Dumper (not loaded)

--- lib/Text/Amuse/Output.pm
+++ lib/Text/Amuse/Output.pm
@@ -187,7 +187,7 @@ sub process {
             push @pieces, $self->manage_example($el);
         }
         else {
-            die "Unrecognized element: " . Dumper($el);
+            die "Unrecognized element: " . $el->type);
         }
     }
     if ($self->fmt eq 'html') {
