7746360166271bf25de87195e98718503dc003c5
Marco Pessotto <melmothx@gmail.com>
2015-12-05 11:08:50 +0100

    Bumped version

--- lib/Text/Amuse.pm
+++ lib/Text/Amuse.pm
@@ -14,11 +14,11 @@ Text::Amuse - Perl module to generate HTML and LaTeX documents from Emacs Muse m
 
 =head1 VERSION
 
-Version 0.52
+Version 0.53
 
 =cut
 
-our $VERSION = '0.52';
+our $VERSION = '0.53';
 
 
 =head1 SYNOPSIS
