e76c0d62d5b839b325d1462a163f0f3b0a9eeb13
Marco Pessotto <melmothx@gmail.com>
2018-09-29 16:05:54 +0200

    Bump to 1.24

--- lib/Text/Amuse.pm
+++ lib/Text/Amuse.pm
@@ -13,11 +13,11 @@ Text::Amuse - Generate HTML and LaTeX documents from Emacs Muse markup.
 
 =head1 VERSION
 
-Version 1.23
+Version 1.24
 
 =cut
 
-our $VERSION = '1.23';
+our $VERSION = '1.24';
 
 
 =head1 SYNOPSIS
