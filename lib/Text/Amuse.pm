40870037b15e180e1e729a07c155edb43610d340
Marco Pessotto <melmothx@gmail.com>
2020-02-26 08:05:00 +0100

    Implement named anchors in ToC
    Close #58

--- lib/Text/Amuse.pm
+++ lib/Text/Amuse.pm
@@ -235,10 +235,11 @@ sub toc_as_html {
     foreach my $item (@toc) {
         next unless $item->{index}; # skip the 0 one, is dummy
         next unless length $item->{string}; # skip empty one at output level
+        my $anchor = $item->{named} ? $item->{named}  : 'toc' . $item->{index};
         my $line = qq{<p class="tableofcontentline toclevel} .
           $item->{level} . qq{"><span class="tocprefix">} .
           '&#160;&#160;' x  $item->{level} . "</span>" .
-            qq{<a href="#toc} . $item->{index} . qq{">} .
+            qq{<a href="#} . $anchor . qq{">} .
               $item->{string} . "</a></p>";
         push @out, $line;
     }
