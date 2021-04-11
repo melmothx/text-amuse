bd56f3c700a29967aa554da2e05fb9730f485138
Marco Pessotto <marco@pessotto.hr>
2021-04-11 08:58:43 +0200

    Version 1.76

--- lib/Text/Amuse.pm
+++ lib/Text/Amuse.pm
@@ -13,11 +13,11 @@ Text::Amuse - Generate HTML and LaTeX documents from Emacs Muse markup.
 
 =head1 VERSION
 
-Version 1.75
+Version 1.76
 
 =cut
 
-our $VERSION = '1.75';
+our $VERSION = '1.76';
 
 
 =head1 SYNOPSIS
